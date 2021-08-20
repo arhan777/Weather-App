57d839242cd238d2e611a69049863e4f70ac2623
arhan777 <arhanshekhar@gmail.com>
2021-08-20 12:53:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+let loc = document.getElementById("location");
+let tempicon = document.getElementById("temp-icon");
+let tempvalue = document.getElementById("temp-value");
+let climate = document.getElementById("climate");
+let iconfile;
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    getWeather(searchInput.value);
+    searchInput.value = '';
+
+});
+
+const getWeather = async (city) => {
+    try {
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=450eec947ba588375646375198c337f7`;
+        console.log(URI);
+        const response = await fetch(URI);
+        const weatherData = await response.json();
+        console.log(weatherData);
+        const { name } = weatherData;
+        const { feels_like } = weatherData.main;
+        const { id, main } = weatherData.weather[0];
+        loc.textContent = name;
+        climate.textContent = main;
+        tempvalue.textContent = Math.round(feels_like - 273);
+        if (id < 300 && id > 200) {
+            tempicon.src = "./icons/thunderstorm.png"
+        }
+        else if (id < 400 && id > 300) {
+            tempicon.src = "./icons/cloud.png"
+        }
+        else if (id < 600 && id > 500) {
+            tempicon.src = "./icons/rain.png"
+        }
+
+        else if (id < 700 && id > 600) {
+            tempicon.src = "./icons/snowman.png"
+        }
+
+        else if (id < 800 && id > 700) {
+            tempicon.src = "./icons/cloud.png"
+        }
+        else if (id == 800) {
+            tempicon.src = "./icon/cloud-and-sun.png"
+        }
+    }
+    catch (error) {
+        alert('city not found');
+    }
+
+
+};
+
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const proxy = "https://corsanywhere.herokuapp.com/";
+
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=450eec947ba588375646375198c337f7`
+
+
+            fetch(api).then((response) => {
+                return response.json();
+
+            })
+
+
+                .then(data => {
+                    // console.log(data);
+                    const { name } = data;
+                    const { feels_like } = data.main;
+                    const { id, main } = data.weather[0];
+
+                    loc.textContent = name;
+                    climate.textContent = main;
+                    tempvalue.textContent = Math.round(feels_like - 273);
+                    if (id < 300 && id > 200) {
+                        tempicon.src = "./icons/thunderstorm.png"
+                    }
+                    else if (id < 400 && id > 300) {
+                        tempicon.src = "./icons/cloud.png"
+                    }
+                    else if (id < 600 && id > 500) {
+                        tempicon.src = "./icons/rain.png"
+                    }
+
+                    else if (id < 700 && id > 600) {
+                        tempicon.src = "./icons/snowman.png"
+                    }
+
+                    else if (id < 800 && id > 700) {
+                        tempicon.src = "./icons/cloud.png"
+                    }
+                    else if (id == 800) {
+                        tempicon.src = "./icon/cloud-and-sun.png"
+                    }
+
+                    console.log(data);
+
+                })
+        }
+
+        )
+    }
+
+})
